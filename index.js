7139ed6d4480e4336f8ae1f59936928d2eb51fe4
Ruchika Jain <jainruchi468@gmail.com>
2024-06-05 09:55:53 +0530

    Changed msg

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const message = ["Hello, ", "I've learnt react"];
+const message = ["Hello, ", "I've learnt Git"];
 const html = `<span class="red">${message[0]}</span> <span class="blue">${message[1]}</span>`;
 document.getElementById("msg").innerHTML = html;
